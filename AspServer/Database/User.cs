cfc5df07b034e28e3cf05653e36c95c26002ae94
Lucas Vieira <lucasvg@poli.ufrj.br>
2019-10-21 19:34:32 -0300

    Creates first workable version of the api

---
+++ AspServer/Database/User.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace AspServer {
+    public class User {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Coordinate> Coordinates { get; set; }
+    }
+}
---
+++ AspServer/GraphQL/CoordinateType.cs
@@ -0,0 +1,13 @@
+﻿using GraphQL.Types;
+
+namespace AspServer.GraphQL {
+    public class CoordinateType : ObjectGraphType<Coordinate>{
+        public CoordinateType() {
+            Name = "Coordinate";
+
+            Field(x => x.Id, type: typeof(IdGraphType)).Description("The id of the coordinate");
+            Field(x => x.Latitude).Description("The latitude of the coordinate");
+            Field(x => x.Longitude).Description("The longitude of the coordinate");
+        }
+    }
+}

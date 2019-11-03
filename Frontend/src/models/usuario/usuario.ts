176c95e1b82b93b8d94e6b556f077225d162f3f6
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 17:17:50 -0300

    Definindo construtor para classe usuário

--- Frontend/src/models/usuario/usuario.ts
+++ Frontend/src/models/usuario/usuario.ts
@@ -1,14 +1,38 @@
 import { Dificuldade } from '../dificuldade/dificuldade';
 import { Coordenada } from '../coordenada/coordenada';
+import { Pontuacao } from '../pontuacao/pontuacao';
 
 export class Usuario {
   public id: number;
   public nomeDeUsuario: string;
   public pontuacao: number;
   public dificuldade: Dificuldade;
+  public coordenadas: Array<Coordenada>;
 
-  public adicionarAmigo(usuario: Usuario): boolean {}
-  public seguirUsuario(usuario: Usuario): boolean {}
-  public bloquearUsuario(usuario: Usuario): boolean {}
-  public enviarCoordenada(novaCoordenada: Coordenada): boolean {}
+  constructor({
+    id,
+    nomeDeUsuario,
+    dificuldade,
+    coordenadas,
+    pontuacao
+  }: {
+    id: number;
+    nomeDeUsuario: string;
+    dificuldade: Dificuldade;
+    coordenadas?: Array<Coordenada>;
+    pontuacao?: number;
+  }) {
+    this.id = id;
+    this.nomeDeUsuario = nomeDeUsuario;
+    this.dificuldade = dificuldade;
+    this.coordenadas = coordenadas ? coordenadas : [];
+    this.pontuacao = pontuacao
+      ? pontuacao
+      : new Pontuacao(this.coordenadas).calcularPontuacao(this.dificuldade);
+  }
+
+  public adicionarAmigo(usuario: Usuario) {}
+  public seguirUsuario(usuario: Usuario) {}
+  public bloquearUsuario(usuario: Usuario) {}
+  public enviarCoordenada(novaCoordenada: Coordenada) {}
 }

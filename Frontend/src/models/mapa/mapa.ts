02f06edabf4eaedbfb643553a099b4366c9db35f
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 17:19:57 -0300

    Redefinindo estrutura da classe mapa de acordo com diagrama

--- Frontend/src/models/mapa/mapa.ts
+++ Frontend/src/models/mapa/mapa.ts
@@ -2,9 +2,13 @@ import { Coordenada } from '../coordenada/coordenada';
 
 export class Mapa {
   public coordenadaFoco: Coordenada;
+  public coordenadasUsuario: Array<Coordenada>;
 
-  constructor(arrayCoordenadas: Coordenada) {
+  constructor(arrayCoordenadas: Array<Coordenada>) {
+    this.coordenadasUsuario = arrayCoordenadas;
     this.coordenadaFoco = arrayCoordenadas[arrayCoordenadas.length - 1];
   }
-  public renderizarNuvem(pontosExplorados: Array<Coordenada>) {}
+  public renderizarNuvem(pontosExplorados: Array<Coordenada>) {
+    console.log('Renderizando fog...');
+  }
 }

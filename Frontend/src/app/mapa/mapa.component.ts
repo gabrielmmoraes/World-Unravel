90808f23107c95852c166684931dbd9bad97ef48
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 17:20:20 -0300

    Implementando classe mapa na tela principal e testando classe de pontuação

--- Frontend/src/app/mapa/mapa.component.ts
+++ Frontend/src/app/mapa/mapa.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Mapa } from '../../models/mapa/mapa';
+import { Pontuacao } from 'src/models/pontuacao/pontuacao';
+import { Dificuldade } from 'src/models/dificuldade/dificuldade';
 
 @Component({
   selector: 'app-mapa',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mapa.component.css']
 })
 export class MapaComponent implements OnInit {
-  public latitude = 51.678418;
-  public longitude = 7.809007;
+  private googleMaps: Mapa;
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const arrayCoordenadas = [{ latitude: 51.678418, longitude: 7.809007 }];
+    this.googleMaps = new Mapa(arrayCoordenadas);
+    this.googleMaps.renderizarNuvem(arrayCoordenadas);
+    const a = new Pontuacao([
+      { latitude: 52.2296756, longitude: 21.0122287 },
+      { latitude: 52.406374, longitude: 16.9251681 },
+      { latitude: 52.2296756, longitude: 21.0122287 }
+    ]);
+    console.log(a.calcularPontuacao(Dificuldade.FACIL));
+  }
 }

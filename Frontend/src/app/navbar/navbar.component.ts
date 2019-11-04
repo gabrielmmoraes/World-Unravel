317ca04c121c113e0144ea06e449269c7a9a8197
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 21:14:28 -0300

    Removendo opções de abrir e fechar navegador

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -9,12 +9,4 @@ export class NavbarComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
-
-  public openNav() {
-    document.getElementById('mySidenav').style.width = '350px';
-  }
-
-  public closeNav() {
-    document.getElementById('mySidenav').style.width = '0';
-  }
 }

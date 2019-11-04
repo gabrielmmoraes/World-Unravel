13563c88b3d76afe79e5c63874ea06ab5ca42b95
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 21:13:05 -0300

    Adicionando cálculo de pontuação do grupo

--- Frontend/src/models/grupo/grupo.ts
+++ Frontend/src/models/grupo/grupo.ts
@@ -3,6 +3,7 @@ import { Usuario } from '../usuario/usuario';
 export class Grupo {
   public nome: string;
   public integrantes: Array<Usuario>;
+  public pontuacao: number;
 
   constructor({
     nome,
@@ -13,6 +14,9 @@ export class Grupo {
   }) {
     this.nome = nome;
     this.integrantes = integrantes;
+    this.pontuacao = this.integrantes.reduce((acumulador, integrante) => {
+      return acumulador + integrante.pontuacao;
+    }, 0);
   }
 
   public adicionarMembro(novoMembro: Usuario) {}

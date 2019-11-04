8574690b57734ba02200d37e1545504da07ef222
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 21:12:45 -0300

    Documentando e aplicando arredondamento na pontuação

--- Frontend/src/models/pontuacao/pontuacao.ts
+++ Frontend/src/models/pontuacao/pontuacao.ts
@@ -10,19 +10,33 @@ export class Pontuacao {
     this.coordenadas = coordenadas;
   }
 
+  /**
+   * Percorre todo o array de coordenadas e calcula a distância entre pares de pontos.
+   * A distância é registrada em metros e aplicada o peso de acordo com a dificuldade.
+   * @param dificuldade Define o peso que será aplicado nas distâncias
+   */
   public calcularPontuacao(dificuldade: Dificuldade): number {
-    return this.coordenadas.reduce((acumulador, coordenada, index) => {
-      if (index < this.coordenadas.length - 1) {
-        return (
-          acumulador +
-          distanciaLatLng(coordenada, this.coordenadas[index + 1]) * dificuldade
-        );
-      } else {
-        return acumulador;
-      }
-    }, 0);
+    return Math.round(
+      this.coordenadas.reduce((acumulador, coordenada, index) => {
+        if (index < this.coordenadas.length - 1) {
+          return (
+            acumulador +
+            distanciaLatLng(coordenada, this.coordenadas[index + 1]) *
+              dificuldade *
+              1000
+          );
+        } else {
+          return acumulador;
+        }
+      }, 0)
+    );
   }
 
+  /**
+   * Adiciona coordenada ao array do usuário e recalcula a pontuação
+   * @param usuario Usuário que terá coordenada adicionada
+   * @param novaCoordenada Coordenada a ser adicionada
+   */
   public atualizarPontuacao({
     usuario,
     novaCoordenada
@@ -30,6 +44,7 @@ export class Pontuacao {
     usuario: Usuario;
     novaCoordenada: Coordenada;
   }): number {
-    return 100;
+    usuario.coordenadas.push(novaCoordenada);
+    return this.calcularPontuacao(usuario.dificuldade);
   }
 }

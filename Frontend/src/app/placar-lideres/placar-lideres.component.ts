9d04ed05f175fa868790bad0b1d2b9e9df4e894e
Gabriel Mendes <gabrielm.moraes@hotmail.com>
2019-11-03 21:15:54 -0300

    Adicionando funcionalidade no placar de líderes

--- Frontend/src/app/placar-lideres/placar-lideres.component.ts
+++ Frontend/src/app/placar-lideres/placar-lideres.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/models/usuario/usuario';
+import { mockUsuarios } from 'src/mocks/usuario-mock';
 
 @Component({
   selector: 'app-placar-lideres',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./placar-lideres.component.css']
 })
 export class PlacarLideresComponent implements OnInit {
+  private usuarios: Array<Usuario>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.usuarios = mockUsuarios.sort((usuario1, usuario2) => {
+      return usuario2.pontuacao - usuario1.pontuacao;
+    });
   }
 
+  ngOnInit() {}
 }
